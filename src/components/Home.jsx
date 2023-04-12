5bf6ab95534d27e9087366683945978342059dd4
Rajat <rajrajat876@gmail.com>
2023-04-12 11:21:09 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { getPosts } from "../api/post";
+
+const pageNo = 0;
+const POST_LIMIT = 9;
+
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const fetchPosts = async () => {
+    const { error, posts } = await getPosts(pageNo, POST_LIMIT);
+
+    if (error) return console.log(error);
+    console.log(posts);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      {posts.map((post) => {
+        return <div>{post.title}</div>;
+      })}
+    </div>
+  );
+}
